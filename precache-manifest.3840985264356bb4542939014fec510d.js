faf004be35e5e6417c2ca88943f30e0f41212fdf
ismaelsimonmoreno <ismaelsimonmoreno@MacBook-Air-de-ismael.local>
2020-05-17 19:35:27 +0200

    Updates

---
+++ precache-manifest.3840985264356bb4542939014fec510d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c92fc8f9b6f581ca36f2a6ca7f391af",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "eb065ee89a3e417ba598",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a1bc7dcb0ddda7c2efa0",
+    "url": "/my-app/static/js/2.d6d43cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.d6d43cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb065ee89a3e417ba598",
+    "url": "/my-app/static/js/main.3b5fa8a0.chunk.js"
+  },
+  {
+    "revision": "681c48388c01c987c399",
+    "url": "/my-app/static/js/runtime-main.17e589df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
